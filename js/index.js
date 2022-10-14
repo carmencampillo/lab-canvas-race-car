8a001aecb38f5bd8d4af0260d253104f8f6125e8
carmencampillo <carmencs12@hotmail.com>
2022-10-14 07:45:28 +0100

    labcar

--- js/index.js
+++ js/index.js
@@ -5,3 +5,31 @@ window.onload = () => {
 
   function startGame() {}
 };
+const canvas = document.querySelector("canvas");
+canvas.style.border = "2px solid black";
+const ctx = canvas.getContext("2d");
+const background = new Image();
+background.src = "../images/road.png";
+const car = new Image();
+car.src = "../images/car.png";
+let speed = 0;
+let carX = 200;
+let carY = 525;
+const carWidth = 100;
+const carHeight = 150;
+window.onload = () => {
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  // ctx.drawImage(car, carX, carY, carWidth, carHeight);
+  function drawCar(carX, carY, carWidth, carHeight) {
+    ctx.drawImage(car, carX, carY, carWidth, carHeight);
+  }
+drawCar(250, 300, carWidth, carHeight)
+  document.addEventListener("keydown", (event) => {
+    if (event.key == "ArrowLeft") {
+      console.log("keyleft");
+    } else if (event.key == "ArrowRight") {
+      console.log("Keyright");
+    }
+  });
+}
+
